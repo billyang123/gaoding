21c6a23a9113b1f5bb79895e4951b3492e9e34ea
yangbinbin <yangbinbin_1226@126.com>
2015-11-06 17:33:22 +0800

    commit

---
+++ pages/index.php
@@ -0,0 +1,275 @@
+<link rel="stylesheet" type="text/css" href="/dev/styles/index.css">
+<h3>收复失地</h3>
+<div class="taglist cf">
+	<a href="#" class="tag">公关文案</a>
+	<a href="#" class="tag">事件报道</a>
+	<a href="#" class="tag">活动宣传</a>
+</div>
+<div class="detail-hd-panel">
+	<h3 class="panel-title">量身定制一篇百万量级的公关文案</h3>
+	<div class="gd-price">
+		<span class="rem">¥</span>
+		<span class="price">5000</span>
+		<span class="per">/ 次</span>
+	</div>
+	<div class="detail-hd-type">
+		<span><i class="iconfont">&#xe60a;</i> 双方协商</span>
+		<span><i class="iconfont">&#xe603;</i> 线上</span>
+	</div>
+	<div class="detail-hd-num mt20">
+		<em>80</em> 收藏 <i></i><em>20</em> 购买
+	</div>
+</div>
+<div class="gdm-dianpu-hd">
+	<div class="userImgBox">
+		<img src="http://renheuserfaceimages.b0.upaiyun.com/userface/userdefinednew/1442/2964/89/userface_6783482.jpg" class="userimg circle">
+	</div>
+	<h3 class="gdm-title">杨波</h3>
+	<p>阿里巴巴  /  新媒体运营经理</p>
+	<button class="btn btn-danger round btw180 mt20">收藏</button>
+	<p class="cf">
+		<span class="fl address"><i class="iconfont">&#xe603;</i> 杭州</span>
+		<span class="fr favnum">80 <i class="iconfont">&#xe604;</i></span>
+	</p>
+</div>
+<ul class="well-list">
+	<li><span>擅长领域：</span>联网金融、电子商务、社交APP推广</li>
+	<li><span>服务区域：</span>不限</li>
+	<li><span>擅长领域：</span>互联网金融、电子商务、社交APP推广 服务区域：不限</li>
+</ul>
+<div class="panel">
+	<div class="panel-hd">
+		<h3 class="panel-title">交易流程</h3>
+	</div>
+	<div class="panel-bd panel-bd-list">
+		<div class="item">
+			<h3 class="cf"><span class="fl">1 . 意向达成</span><span class="fr">付款30%</span></h3>
+			<p>达成合作意向，预付30%的定金</p>
+			<hr>
+		</div>
+		<div class="item">
+			<h3 class="cf"><span class="fl">1 . 意向达成</span><span class="fr">付款30%</span></h3>
+			<p>达成合作意向，预付30%的定金</p>
+		</div>
+		
+	</div>
+</div>
+<div class="panel">
+	<div class="panel-hd">
+		<h3 class="panel-title">评价(8)</h3>
+	</div>
+	<div class="panel-bd panel-bd-appraises panel-bd-list">
+		<div class="media">
+			<div class="media-left">
+				<img src="http://renheuserfaceimages.b0.upaiyun.com/userface/userdefinednew/1442/2964/89/userface_6783482.jpg" class="userimg circle">
+			</div>
+			<div class="media-body">
+				<h3 class="media-heading">杨波<span>阿里巴巴  /  新媒体运营经理</span></h3>
+				<p>用户评价文案，用户评价文案，用户评价文案，用户评价文案，用户评价文案，用户评价文案。</p>
+				<p class="cf appra-stars">
+					<span class="fl">评价
+						<em class="iconfont gd-red">&#xe610;</em>
+						<em class="iconfont gd-red">&#xe610;</em>
+						<em class="iconfont gd-red">&#xe610;</em>
+					</span>
+					<time class="fr time">2010-10-10</time>
+				</p>
+
+			</div>
+			<hr>
+		</div>
+		<div class="media">
+			<div class="media-left">
+				<img src="http://renheuserfaceimages.b0.upaiyun.com/userface/userdefinednew/1442/2964/89/userface_6783482.jpg" class="userimg circle">
+			</div>
+			<div class="media-body">
+				<h3 class="media-heading">杨波<span>阿里巴巴  /  新媒体运营经理</span></h3>
+				<p>用户评价文案，用户评价文案，用户评价文案，用户评价文案，用户评价文案，用户评价文案。</p>
+				<p class="cf appra-stars">
+					<span class="fl">评价
+						<em class="iconfont gd-red">&#xe610;</em>
+						<em class="iconfont gd-red">&#xe610;</em>
+						<em class="iconfont gd-red">&#xe610;</em>
+					</span>
+					<time class="fr time">2010-10-10</time>
+				</p>
+
+			</div>
+			<hr>
+		</div>
+		<div class="media">
+			<div class="media-left">
+				<img src="http://renheuserfaceimages.b0.upaiyun.com/userface/userdefinednew/1442/2964/89/userface_6783482.jpg" class="userimg circle">
+			</div>
+			<div class="media-body">
+				<h3 class="media-heading">杨波<span>阿里巴巴  /  新媒体运营经理</span></h3>
+				<p>用户评价文案，用户评价文案，用户评价文案，用户评价文案，用户评价文案，用户评价文案。</p>
+				<p class="cf appra-stars">
+					<span class="fl">评价
+						<em class="iconfont gd-red">&#xe610;</em>
+						<em class="iconfont gd-red">&#xe610;</em>
+						<em class="iconfont gd-red">&#xe610;</em>
+					</span>
+					<time class="fr time">2010-10-10</time>
+				</p>
+
+			</div>
+		</div>
+	</div>
+	<div class="panel-more text-center">更多评价</div>
+</div>
+<div class="panel">
+	<div class="panel-hd">
+		<h3 class="panel-title">成功案例</h3>
+	</div>
+	<div class="panel-bd panel-bd-list">
+		<div class="item">
+			<h3>为什么大家都等着看小米和阿里的笑话</h3>
+			<span class="iconfont icon-right">&#xe60c;</span>
+			<hr>
+		</div>
+		<div class="item">
+			<h3>为什么大家都等着看小米和阿里的笑话</h3>
+			<span class="iconfont icon-right">&#xe60c;</span>
+			<hr>
+		</div>
+		<div class="item">
+			<h3>为什么大家都等着看小米和阿里的笑话</h3>
+			<span class="iconfont icon-right">&#xe60c;</span>
+		</div>
+	</div>
+</div>
+<div class="panel">
+	<div class="panel-hd">
+		<h3 class="panel-title">服务内容</h3>
+	</div>
+	<div class="panel-bd">
+		<p>
+			情怀，有人说是理想主义。新时代，情怀是一种梦想的寄托，当无处发泄的生活压力蜂拥而上，情怀被追捧。从粗放式的广告走出来，精细化的广告拥有情怀，越来越多的走心文案被追捧。营销， 是发自内心的。
+			<br>1. PR推广策略分析，量身定制最优方案
+			<br>2. 根据要求，撰写相关文案
+			<br>3. 多个主要自媒体平台（今日头条、百度百家等）发布
+			<br>4. 部分自媒体平台首页推荐（可能）
+			<br>5. 不刷量不做假，认真做事
+		</p>
+	</div>
+</div>
+<div class="media media-card border-bottom">
+	<div class="media-left">
+		<img src="http://renheuserfaceimages.b0.upaiyun.com/userface/userdefinednew/1442/2964/89/userface_6783482.jpg" class="userimg circle">
+	</div>
+	<div class="media-body">
+		<h3 class="media-heading">杨波</h3>
+		<p>杨波      阿里巴巴  /  新媒体运营经理</p>
+		<span class="iconfont icon-right">&#xe60c;</span>
+	</div>
+</div>
+<div class="panel-group">
+	<div class="panel">
+		<div class="panel-bd">
+			<h3 class="panel-title">量身定制一篇百万量级的公关文案</h3>
+			<div class="taglist cf">
+				<span class="tag">公关文案</span>
+				<span class="tag">事件报道</span>
+				<span class="tag">活动宣传</span>
+			</div>
+			<div class="gd-price">
+				<span class="rem">¥</span>
+				<span class="price">5000</span>
+				<span class="per">/ 次</span>
+			</div>
+			<div class="gdt-card">
+				<span class="userimgbox"><img class="userimg circle" src="http://renheuserfaceimages.b0.upaiyun.com/userface/userdefinednew/1442/2964/89/userface_6783482.jpg"></span>
+				<p>杨波   阿里巴巴  /  新媒体运营经理</p>
+			</div>
+		</div>
+	</div>
+	<div class="panel">
+		<div class="panel-bd">
+			<h3 class="panel-title">量身定制一篇百万量级的公关文案</h3>
+			<div class="taglist cf">
+				<span class="tag">公关文案</span>
+				<span class="tag">事件报道</span>
+				<span class="tag">活动宣传</span>
+			</div>
+			<div class="gd-price">
+				<span class="rem">¥</span>
+				<span class="price">5000</span>
+				<span class="per">/ 次</span>
+			</div>
+			<div class="gdt-card">
+				<span class="userimgbox"><img class="userimg circle" src="http://renheuserfaceimages.b0.upaiyun.com/userface/userdefinednew/1442/2964/89/userface_6783482.jpg"></span>
+				<p>杨波   阿里巴巴  /  新媒体运营经理</p>
+			</div>
+		</div>
+	</div>
+	<div class="panel">
+		<div class="panel-bd">
+			<h3 class="panel-title">量身定制一篇百万量级的公关文案</h3>
+			<div class="taglist cf">
+				<a href="#" class="tag">公关文案</a>
+				<a href="#" class="tag">事件报道</a>
+				<a href="#" class="tag">活动宣传</a>
+			</div>
+			<div class="gd-price">
+				<span class="rem">¥</span>
+				<span class="price">5000</span>
+				<span class="per">/ 次</span>
+			</div>
+			<div class="gdt-card">
+				<a href="#" class="userimgbox"><img class="userimg circle" src="http://renheuserfaceimages.b0.upaiyun.com/userface/userdefinednew/1442/2964/89/userface_6783482.jpg"></a>
+				<p>杨波   阿里巴巴  /  新媒体运营经理</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="panel-group">
+	<div class="panel panel-detail-intro">
+		<div class="panel-hd">
+			<h3 class="panel-title">服务推荐</h3>
+		</div>
+		<div class="panel-bd">
+			<h3 class="panel-title">量身定制一篇百万量级的公关文案</h3>
+			<div class="gd-price">
+				<span class="rem">¥</span>
+				<span class="price">5000</span>
+				<span class="per">/ 次</span>
+			</div>
+			<div class="btn-right"><button class="btn btn-danger round btw140">详细</button></div>
+		</div>
+	</div>
+</div>
+<div class="nav-fix-bt detail-fix">
+	<div class="item">
+		<div class="itemlink">
+			<i class="icon iconfont">&#xe607;</i>
+			<br><span class="txt">咨询</span>
+		</div>
+	</div>
+	<div class="item">
+		<div class="itemlink">
+			<i class="icon iconfont">&#xe609;</i>
+			<br><span class="txt">店铺</span>
+		</div>
+	</div>
+	<div class="item">
+		<div class="itemlink">
+			<i class="icon iconfont">&#xe604;</i>
+			<br><span class="txt">收藏</span>
+		</div>
+	</div>
+	<div class="item buy">
+		<div class="itemlink">
+			<span>购买</span>
+		</div>
+	</div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+gd_dianpu_headbg_20151106_750x460.jpg
